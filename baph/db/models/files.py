e4422e6e6c297ecaf32ff254959fee0296bb54c2
Gerald Thibault <dieselmachine@gmail.com>
2013-03-28 22:10:12 +0000

    upgraded default decl_base (caching, etc), initial commit of forms module for auto-generating forms from SQLA models, models changed from flat file to fodler-based module (in order to add subfiles), object cloning support, base Meta options for SQLA models

---
+++ baph/db/models/files.py
@@ -0,0 +1,46 @@
+from django.core.files.images import ImageFile
+import hashlib
+
+def md5_checksum(filepath):
+    if type(filepath) == str:
+        fh = open(filepath, 'rb')
+    elif hasattr(filepath, 'read'):
+        fh = filepath
+    else:
+        raise Exception('Invalid type for md5_checksum: %s' % type(filepath))
+    m = hashlib.md5()
+    while True:
+        data = fh.read(8192)
+        if not data:
+            break
+        m.update(data)
+    return m.hexdigest()
+
+class ChecksumImageFile(ImageFile):
+
+    def __init__(self, *args, **kwargs):
+        self.storage = kwargs.pop('storage', None)
+        super(ChecksumImageFile, self).__init__(*args, **kwargs)
+
+    @property
+    def checksum(self):
+        if not hasattr(self, '_checksum'):
+            if hasattr(self.file, 'checksum'):
+                self._checksum = self.file.checksum
+            else:
+                self._checksum = md5_checksum(self)
+        return self._checksum
+        
+    def save(self):
+        if not self.storage:
+            raise Exception('ChecksumImageFile.save() cannot be called unless '
+                'it was initialized with the "storage" kwarg')
+        self.name = self.storage.save(self.name, self.file)
+        return self.name
+        
+    @property
+    def url(self):
+        if not self.storage:
+            raise Exception('ChecksumImageFile.url cannot be called unless '
+                'it was initialized with the "storage" kwarg')
+        return self.storage.url(self.name)
---
+++ baph/db/models/mixins.py
@@ -0,0 +1,18 @@
+import datetime
+
+from sqlalchemy import Column, DateTime, func
+from sqlalchemy.ext.declarative import declared_attr
+
+
+class TimestampMixin(object):
+
+    @declared_attr
+    def added(cls):
+        return Column(DateTime, default=datetime.datetime.now, nullable=False,
+            info={'readonly': True})
+
+    @declared_attr
+    def modified(cls):
+        return Column(DateTime, default=datetime.datetime.now,
+            onupdate=datetime.datetime.now, info={'readonly': True})
+
