592445f3e12e82e2c6e1f4c70ed533d9a7744a30
Gerald Thibault <dieselmachine@gmail.com>
2013-03-28 20:05:39 +0000

    added random_string function to utils/strings.py, autoflush is now OFF by default

---
+++ baph/utils/strings.py
@@ -0,0 +1,17 @@
+import random
+import string
+import time
+
+
+def random_string(size=16, chars=None):
+    if chars is None:
+        chars = []
+        chars.extend([i for i in string.ascii_letters])
+        chars.extend([i for i in string.digits])
+        chars.extend([i for i in '!@#$%&*()-_=+[{}]~^,<.>;:/?'])
+    s = ''
+    for i in range(size):
+        s += chars[random.randint(0, len(chars) - 1)]
+        random.seed = int(time.time())
+        random.shuffle(chars)
+    return s
