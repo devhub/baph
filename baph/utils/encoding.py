5e37791eb177bd8b60bc428180453e699eb25b83
Gerald Thibault <dieselmachine@gmail.com>
2013-08-13 00:21:11 +0000

    addition of function to escape html entities

--- baph/utils/encoding.py
+++ baph/utils/encoding.py
@@ -227,3 +227,45 @@ def get_system_encoding():
     return encoding
 
 DEFAULT_LOCALE_ENCODING = get_system_encoding()
+
+# baph additions
+
+def unescape(text):
+   """Removes HTML or XML character references 
+      and entities from a text string.
+   @param text The HTML (or XML) source text.
+   @return The plain text, as a Unicode string, if necessary.
+   from Fredrik Lundh
+   2008-01-03: input only unicode characters string.
+   http://effbot.org/zone/re-sub.htm#unescape-html
+   """
+   def fixup(m):
+      text = m.group(0)
+      if text[:2] == "&#":
+         # character reference
+         try:
+            if text[:3] == "&#x":
+               return unichr(int(text[3:-1], 16))
+            else:
+               return unichr(int(text[2:-1]))
+         except ValueError:
+            print "Value Error"
+            pass
+      else:
+         # named entity
+         # reescape the reserved characters.
+         try:
+            if text[1:-1] == "amp":
+               text = "&amp;amp;"
+            elif text[1:-1] == "gt":
+               text = "&amp;gt;"
+            elif text[1:-1] == "lt":
+               text = "&amp;lt;"
+            else:
+               print text[1:-1]
+               text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+         except KeyError:
+            print "keyerror"
+            pass
+      return text # leave as is
+   return re.sub("&#?\w+;", fixup, text)
