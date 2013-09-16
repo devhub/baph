f27c7b93ca215b33ff0233630abe599050e33531
Gerald Thibault <dieselmachine@gmail.com>
2013-09-16 23:39:51 +0000

    temp

--- baph/auth/backends.py
+++ baph/auth/backends.py
@@ -38,3 +38,36 @@ class SQLAlchemyBackend(object):
         if not session:
             session = orm.sessionmaker()
         return session.query(User).get(user_id)
+
+class MultiSQLAlchemyBackend(SQLAlchemyBackend):
+    """Backend which auths via username or email"""
+    
+    def authenticate(self, identification, password=None, check_password=True):
+        session = orm.sessionmaker()
+        org_key = Organization.resource_name + '_id'
+        try:
+            django.core.validators.validate_email(identification)
+            if auth_settings.BAPH_AUTH_UNIQUE_EMAIL:
+                filters = {'email': identification}
+            elif auth_settings.BAPH_AUTH_UNIQUE_ORG_EMAIL:
+                filters = {
+                    'email': identification,
+                    org_key: Organization.get_current_id(),
+                    }
+            user = session.query(User).filter_by(**filters).first()
+            if not user: return None
+        except django.core.validators.ValidationError:
+            if auth_settings.BAPH_AUTH_UNIQUE_USERNAME:
+                filters = {User.USERNAME_FIELD: identification}
+            elif auth_settings.BAPH_AUTH_UNIQUE_ORG_USERNAME:
+                filters = {
+                    User.USERNAME_FIELD: identification,
+                    org_key: Organization.get_current_id(),
+                    }
+            user = session.query(User).filter_by(**filters).first()
+            if not user: return None
+        if check_password:
+            if user.check_password(password):
+                return user
+            return None
+        else: return user    
