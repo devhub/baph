efe98aa38b069d7796a99bef81631d0d4034435c
Daniel Rust <dlrust@gmail.com>
2013-09-24 11:57:49 -0700

    Fixes for missing templates and fixtures?

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@
 from setuptools import setup, find_packages
 
 setup(name='baph',
-      version='0.3.2',
+      version='0.3.4',
       install_requires=[
           'Coffin',
           'Django >= 1.5',
           'SQLAlchemy >= 0.9.0',
       ],
+      include_package_data=True,
       package_data={
           '': ['*.rst'],
       },
