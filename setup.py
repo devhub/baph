086d35642322fd2312de3f61ee2e62c8107c8824
Daniel Rust <dlrust@gmail.com>
2013-10-09 12:20:03 -0700

    Bumped version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='baph',
-      version='0.3.5',
+      version='0.3.6',
       install_requires=[
           'Coffin',
           'Django >= 1.5',
