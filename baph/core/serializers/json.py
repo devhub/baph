561d94a388380250f4fd2d79b19006183bfcdd0b
JT <dieselmachine@gmail.com>
2014-10-14 18:39:07 +0000

    Changes to avoid deprecation warnings regarding simplejson

--- baph/core/serializers/json.py
+++ baph/core/serializers/json.py
@@ -1,9 +1,17 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 from StringIO import StringIO
+import datetime
+import decimal
+import json
+import sys
 
 from baph.core.serializers.python import Serializer as PythonSerializer
 from baph.core.serializers.python import Deserializer as PythonDeserializer
+from django.core.serializers.base import DeserializationError
 from django.core.serializers.json import DjangoJSONEncoder
-from django.utils import simplejson
+from django.utils import six
 
 
 class Serializer(PythonSerializer):
@@ -12,21 +20,57 @@ class Serializer(PythonSerializer):
     """
     internal_use_only = False
 
+    def start_serialization(self):
+        if json.__version__.split('.') >= ['2', '1', '3']:
+            # Use JS strings to represent Python Decimal instances (ticket #16850)
+            self.options.update({'use_decimal': False})
+        self._current = None
+        self.json_kwargs = self.options.copy()
+        self.json_kwargs.pop('stream', None)
+        self.json_kwargs.pop('fields', None)
+        if self.options.get('indent'):
+            # Prevent trailing spaces
+            self.json_kwargs['separators'] = (',', ': ')
+        self.stream.write("[")
+
     def end_serialization(self):
-        simplejson.dump(self.objects, self.stream, cls=DjangoJSONEncoder,
-            **self.options)
+        if self.options.get("indent"):
+            self.stream.write("\n")
+        self.stream.write("]")
+        if self.options.get("indent"):
+            self.stream.write("\n")
+
+    def end_object(self, obj):
+        # self._current has the field data
+        indent = self.options.get("indent")
+        if not self.first:
+            self.stream.write(",")
+            if not indent:
+                self.stream.write(" ")
+        if indent:
+            self.stream.write("\n")
+        json.dump(self.get_dump_object(obj), self.stream,
+                  cls=DjangoJSONEncoder, **self.json_kwargs)
+        self._current = None
 
     def getvalue(self):
-        if callable(getattr(self.stream, 'getvalue', None)):
-            return self.stream.getvalue()
+        # Grand-parent super
+        return super(PythonSerializer, self).getvalue()
 
 def Deserializer(stream_or_string, **options):
     """
     Deserialize a stream or string of JSON data.
     """
-    if isinstance(stream_or_string, basestring):
-        stream = StringIO(stream_or_string)
-    else:
-        stream = stream_or_string
-    for obj in PythonDeserializer(simplejson.load(stream), **options):
-        yield obj
+    if not isinstance(stream_or_string, (bytes, six.string_types)):
+        stream_or_string = stream_or_string.read()
+    if isinstance(stream_or_string, bytes):
+        stream_or_string = stream_or_string.decode('utf8')
+    try:
+        objects = json.loads(stream_or_string)
+        for obj in PythonDeserializer(objects, **options):
+            yield obj
+    except GeneratorExit:
+        raise
+    except Exception as e:
+        six.reraise(DeserializationError, DeserializationError(e), 
+                    sys.exc_info()[2])
