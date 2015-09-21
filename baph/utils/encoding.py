c0eb0f4ff12c4e2768739c1264d6a4c55a3c7af3
JT <dieselmachine@gmail.com>
2015-09-21 23:18:52 +0000

    removed some print statements, converted some command output from verbosity-based to settings.LOGGING based (to have different logging levels for different types of code), Json type now convers bind param to unicode, to match the col type (to prevent str-vs-unicode warnings)

--- baph/utils/encoding.py
+++ baph/utils/encoding.py
@@ -6,41 +6,29 @@ import re
 
 
 def unescape(text):
-   """Removes HTML or XML character references 
+    """Removes HTML or XML character references 
       and entities from a text string.
-   @param text The HTML (or XML) source text.
-   @return The plain text, as a Unicode string, if necessary.
-   from Fredrik Lundh
-   2008-01-03: input only unicode characters string.
-   http://effbot.org/zone/re-sub.htm#unescape-html
-   """
-   def fixup(m):
-      text = m.group(0)
-      if text[:2] == "&#":
-         # character reference
-         try:
-            if text[:3] == "&#x":
-               return unichr(int(text[3:-1], 16))
-            else:
-               return unichr(int(text[2:-1]))
-         except ValueError:
-            print "Value Error"
-            pass
-      else:
-         # named entity
-         # reescape the reserved characters.
-         try:
-            if text[1:-1] == "amp":
-               text = "&amp;amp;"
-            elif text[1:-1] == "gt":
-               text = "&amp;gt;"
-            elif text[1:-1] == "lt":
-               text = "&amp;lt;"
-            else:
-               print text[1:-1]
-               text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
-         except KeyError:
-            print "keyerror"
-            pass
-      return text # leave as is
-   return re.sub("&#?\w+;", fixup, text)
+    @param text The HTML (or XML) source text.
+    @return The plain text, as a Unicode string, if necessary.
+    from Fredrik Lundh
+    http://effbot.org/zone/re-sub.htm#unescape-html
+    """
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
