abcbb86c11e87590196c21cf0be2c40c75c3d97f
JT <dieselmachine@gmail.com>
2015-11-23 10:57:03 +0000

    instance cache didn't work, cache on the class instead

--- baph/utils/functional.py
+++ baph/utils/functional.py
@@ -4,17 +4,18 @@ import functools
 class memoize(object):
     def __init__(self, func):
         self.func = func
+        self.cache = {}
 
     def __get__(self, instance, type=None):
         if instance is None:
             return self.func
         return functools.partial(self, instance)
 
-    def __call__(self, *args, **kw):
+    def __call__(self, *args, **kwargs):
         instance = args[0]
-        key = (self.func.__name__, args[1:], frozenset(kw.items()))
+        key = (self.func, args[1:], frozenset(kwargs.items()))
         try:
-            res = instance.__dict__[key]
+            res = self.cache[key]
         except KeyError:
-            res = instance.__dict__[key] = self.func(*args, **kw)
+            res = self.cache[key] = self.func(*args, **kwargs)
         return res
