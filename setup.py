84582387a55c26df9c32d82beef7801d252f67cc
Daniel Rust <dlrust@gmail.com>
2016-09-07 14:30:01 -0700

    Bumped version to 0.3.30

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='baph',
-      version='0.3.29',
+      version='0.3.30',
       install_requires=[
           'Coffin >= 0.3.8',
           'Django >= 1.6',
