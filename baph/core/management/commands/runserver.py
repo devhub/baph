755d7d0326e27901551b069bdec4904679789c76
Daniel Rust <dlrust@gmail.com>
2016-12-12 16:26:33 -0800

    Revert "support for custom watchers and handlers when using the runserver autoreloader"
    This reverts commit 2745f4fc9461bee82f0a1648c5471b70bcc9ce18.

--- baph/core/management/commands/runserver.py
+++ baph/core/management/commands/runserver.py
@@ -1,32 +1,19 @@
 from cStringIO import StringIO
-import pyinotify
 
-from django.contrib.staticfiles.management.commands import runserver
+from django.contrib.staticfiles.management.commands.runserver \
+    import Command as RunserverCommand
 
 from baph.core.management.validation import get_validation_errors
-from baph.utils import autoreload
 
 
-class Command(runserver.Command):
-
-  def validate(self, app=None, display_num_errors=False):
-    s = StringIO()
-    num_errors = get_validation_errors(s, app)
-    if num_errors:
-      s.seek(0)
-      error_text = s.read()
-      raise CommandError("One or more models did not validate:\n%s" % error_text)
-    if display_num_errors:
-      self.stdout.write("%s error%s found\n" 
-        % (num_errors, num_errors != 1 and 's' or ''))
-
-  def run(self, *args, **options):
-    """
-    Runs the server, using the autoreloader if needed
-    """
-    use_reloader = options.get('use_reloader')
-
-    if use_reloader:
-      autoreload.main(self.inner_run, args, options)
-    else:
-      self.inner_run(*args, **options)
+class Command(RunserverCommand):
+    def validate(self, app=None, display_num_errors=False):
+        s = StringIO()
+        num_errors = get_validation_errors(s, app)
+        if num_errors:
+            s.seek(0)
+            error_text = s.read()
+            raise CommandError("One or more models did not validate:\n%s" % error_text)
+        if display_num_errors:
+            self.stdout.write("%s error%s found\n" 
+                % (num_errors, num_errors != 1 and 's' or ''))
