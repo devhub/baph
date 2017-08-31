cacaf59106a9b81cb5f8b32f58dc99aa71482893
JT <dieselmachine@gmail.com>
2017-08-31 18:28:30 +0000

    missing global settings

---
+++ baph/conf/global_settings.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+
+
+DEBUG = False
+
+# Local time zone for this installation. All choices can be found here:
+# https://en.wikipedia.org/wiki/List_of_tz_zones_by_name (although not all
+# systems may support all possibilities). When USE_TZ is True, this is
+# interpreted as the default user time zone.
+TIME_ZONE = 'America/Los_Angeles'
+
+INSTALLED_APPS = ()
