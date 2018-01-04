b7912c2f791080f41888c46187ef9a13a3479713
JT <dieselmachine@gmail.com>
2018-01-04 01:49:49 +0000

    baph-specific version of get_wsgi_application

---
+++ baph/core/wsgi.py
@@ -0,0 +1,12 @@
+import baph
+from django.core.handlers.wsgi import WSGIHandler
+
+
+def get_wsgi_application():
+    """
+    The public interface to Django's WSGI support. Return a WSGI callable.
+    Avoids making django.core.handlers.WSGIHandler a public API, in case the
+    internal WSGI implementation changes or moves in the future.
+    """
+    baph.setup()
+    return WSGIHandler()
