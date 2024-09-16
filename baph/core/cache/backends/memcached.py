cdec72ecc1c79c51ff6484d4ff28169a36ed137a
David Welte <dwelte@devhub.com>
2024-09-16 16:19:58 -0700

    Update memcached backend python 3 compatible

--- baph/core/cache/backends/memcached.py
+++ baph/core/cache/backends/memcached.py
@@ -2,26 +2,28 @@ from collections import defaultdict
 from functools import partial
 import operator
 import time
-from urllib import unquote
 
-from cached_property import cached_property
 from django.core.cache.backends.memcached import MemcachedCache
+from six.moves import map, reduce
+from six.moves.urllib.parse import unquote
 
 
-KEY_VALUE_SPLITTER = operator.methodcaller('split', '=', 1)
+def parse_stats_line(line):
+    # line format: STAT <key> <value>
+    return (line[1], line[2])
 
-def parse_metadump(line):
+def parse_metadump_line(line):
     # line format: <key>=<value>*
     split = operator.methodcaller('split', '=', 1)
 
-    data = dict(map(KEY_VALUE_SPLITTER, line))
-    data['key'] = data['key'].encode('utf8')
+    data = dict(list(map(split, line)))
+    data['key'] = unquote(data['key'])
     return data
 
-def parse_cachedump(line, **kwargs):
+def parse_cachedump_line(line, **kwargs):
     data = dict(kwargs)
     key, metadata = line
-    data['key'] = key.encode('utf8')
+    data['key'] = key #.encode('utf8')
     metadata = metadata[1:-1] # strip brackets
     for key in ('size', 'exp'):
         stat, sep, metadata = metadata.partition(';')
@@ -29,20 +31,47 @@ def parse_cachedump(line, **kwargs):
         data[key] = int(value)
     return data
 
-def parse_stat(line):
-    # line format: STAT <key> <value>
-    return (line[1], line[2])
+def parse_stats(response):
+    """ parse the response of a stats command """
+    return list(map(parse_stats_line, response))
+
+def parse_slabs(response):
+    slabs = defaultdict(dict)
+    for key, value in response:
+        # key format: items:<slab_id>:<key>
+        _, slab_id, key = key.split(':', 2)
+        slabs[slab_id][key] = value
+    return slabs
+
+def parse_slab_stats(response):
+    slab_stats = defaultdict(dict)
+    for key, value in parse_stats(response):
+        # key format 1: <slab_id>:<key>
+        # key format 2: <total_key>
+        if ':' in key:
+            slab_id, key = key.split(':')
+        else:
+            slab_id = 'totals'
+        slab_stats[slab_id][key] = value
+    return slab_stats
+
+def parse_cachedump(response):
+    return list(map(parse_cachedump_line, response))
+
+def parse_metadump(response):
+    return list(map(parse_metadump_line, response))
 
 
 class MemcacheServer(object):
     def __init__(self, server):
         self.server = server
+        self.settings = dict(self.get_stats('settings'))
+        if self.settings.get('lru_crawler') == 'yes':
+            self._get_keys = self.get_keys_from_metadump
+        else:
+            self._get_keys = self.get_keys_from_cachedump
 
-    @cached_property
-    def settings(self):
-        return dict(self.get_stats('settings'))
-
-    @cached_property
+    @property
     def slabs(self):
         return self.get_slabs()
 
@@ -74,143 +103,87 @@ class MemcacheServer(object):
                     lines.append(item.split(' ', row_len-1))
         return lines
 
-    # command implementations
-
-    def flush_all(self):
-        """
-        flushes all keys in the cache
-
-        returns None
-        """
-        cmd = 'flush_all'
-        return self.send_command(cmd, expect='OK')
-        #self.delete_many_raw(self.get_keys())
-
-    def delete_many_raw(self, keys):
-        """
-        Deletes the specified keys (does not run them through make_key)
-        """
-        self.server._cache.delete_multi(keys)
-
-    def get_metadump(self, slab_id):
-        cmd = 'lru_crawler metadump %s' % slab_id
-        return map(parse_metadump, self.send_command(cmd))
+    # base commands
 
     def get_stats(self, *args):
-        """
-        returns a list of (key, value) tuples for a stats command
-        """
+        """ returns a list of (key, value) tuples for a stats command """
         cmd = ' '.join(('stats',) + args)
-        return map(parse_stat, self.send_command(cmd, 3))
+        rsp = self.send_command(cmd, 3)
+        return parse_stats(rsp)
 
+    def get_metadump(self, slab_id, limit):
+        # metadump doesn't support a limit param, so we need to handle it
+        limit = limit or None
+        cmd = 'lru_crawler metadump %s' % slab_id
+        rsp = self.send_command(cmd)
+        return parse_metadump(rsp)[:limit]
 
     # STATS command variants
 
     def get_slabs(self):
-        """
-        gets configuration settings for active slabs
-        """
-        slabs = defaultdict(dict)
-        for key, value in self.get_stats('items'):
-            # key format: items:<slab_id>:<key>
-            _, slab_id, key = key.split(':', 2)
-            slabs[slab_id][key] = value
-        return slabs
+        """ gets configuration settings for active slabs """
+        rsp = self.get_stats('items')
+        return parse_slabs(rsp)
 
     def get_slab_stats(self):
-        """
-        gets statistics for active slabs
-        """
-        slab_stats = defaultdict(dict)
-        for key, value in self.get_stats('slabs'):
-            # key format 1: <slab_id>:<key>
-            # key format 2: <total_key>
-            if ':' in key:
-                slab_id, key = key.split(':')
-            else:
-                slab_id = 'totals'
-            slab_stats[slab_id][key] = value
-        return slab_stats
+        """ gets statistics for active slabs """
+        rsp = self.get_stats('slabs')
+        return parse_slab_stats(rsp)
 
     def get_cachedump(self, slab_id, limit):
         limit = limit or 0
         cmd = 'cachedump %s %s' % (slab_id, limit)
-        return map(parse_cachedump, self.get_stats(cmd))
+        rsp = self.get_stats(cmd)
+        return parse_cachedump(rsp)
 
-    def get_slab_keys_from_metadump(self, slab_id, limit=None):
-        """
-        returns all keys in a slab using 'lru_crawler metadump'
-        """
-        # metadump doesn't support a limit param, so we need to handle it
-        items = self.get_metadump(slab_id)
-        return items[:limit]
-
-    def get_slab_keys_from_cachedump(self, slab_id, limit=None):
-        """
-        returns all keys in a slab using 'stats cachedump'
-        """
-        return self.get_cachedump(slab_id, limit)
-
-    def get_slab_keys(self, slab_id, limit=None):
-        """
-        returns all keys in a slab, as a list of dicts
-        """
-        if self.settings.get('lru_crawler', 'no') == 'yes':
-            return self.get_slab_keys_from_metadump(slab_id, limit)
-        else:
-            return self.get_slab_keys_from_cachedump(slab_id, limit)
+    # other commands
 
     def get_keys_from_metadump(self, limit=None):
-        """
-        returns all keys on the server using 'lru_crawler metadump'
-        """
-        return self.get_slab_keys_from_metadump('all', limit)
+        """ returns all keys on the server using 'lru_crawler metadump' """
+        return self.get_metadump('all', limit)
 
     def get_keys_from_cachedump(self, limit=None):
-        """
-        returns all keys on the server using 'stats cachedump'
-        """
-        func = partial(self.get_slab_keys_from_cachedump, limit=limit)
-        return reduce(operator.concat, map(func, self.slabs))
+        """ returns all keys on the server using 'stats cachedump' """
+        func = partial(self.get_cachedump, limit=limit)
+        if self.slabs:
+            return reduce(operator.concat, list(map(func, self.slabs)))
+        return []
 
     def get_keys(self, limit=None, include_expired=False):
-        """
-        returns all keys on the server, as a list of strings
-        """
-        if self.settings.get('lru_crawler', 'no') == 'yes':
-            func = self.get_keys_from_metadump
-        else:
-            func = self.get_keys_from_cachedump
+        """ returns all keys on the server, as a list of strings """
         getter = operator.itemgetter('key')
         ts = time.time()
-        items = func(limit)
+        items = self._get_keys(limit)
         if not include_expired:
-            items = filter(lambda x: x['exp'] > ts, items)
-        return map(getter, items)
+            items = filter(lambda x: float(x['exp']) > ts, items)
+        return list(map(getter, items))
 
 
 class BaphMemcachedCache(MemcachedCache):
-    """
-    An extension of the django memcached Cache class
-    """
+    """ An extension of the django memcached Cache class """
     def __init__(self, server, params):
         super(BaphMemcachedCache, self).__init__(server, params)
         self.version = params.get('VERSION', 0)
         self.alias = params.get('ALIAS', None)
+        self.servers = [MemcacheServer(s) for s in self._cache.servers]
 
-    def __str__(self):
-        return '<BaphMemcachedCache: alias=%r>' % self.alias
+    def get(self, *args, **kwargs):
+        print('GET:', args, kwargs)
+        return super(BaphMemcachedCache, self).get(*args, **kwargs)
 
-    @cached_property
-    def servers(self):
-        return map(MemcacheServer, self._cache.servers)
+    def set(self, *args, **kwargs):
+        print('SET:', args, kwargs)
+        return super(BaphMemcachedCache, self).set(*args, **kwargs)
 
     def get_all_keys(self):
         keys = set()
         for server in self.servers:
+            try:
+                del server.slabs
+            except AttributeError:
+                pass
             keys.update(server.get_keys())
         return keys
 
     def flush_all(self):
-        for server in self.servers:
-            server.flush_all()
+        self.clear()
