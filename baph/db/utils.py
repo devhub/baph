3c1ea27ba5088a746f9789df3ca904a3dc17a6b4
Gerald Thibault <dieselmachine@gmail.com>
2024-12-16 16:31:14 -0800

    Orm middleware commit (#65)
    * commit instead of flush during orm middleware
    
    * refactored syncdb command to handle inherited tables not explicitly attached to a model.

--- baph/db/utils.py
+++ baph/db/utils.py
@@ -5,6 +5,21 @@ from django.db.utils import ConnectionHandler
 DEFAULT_DB_ALIAS = 'default'
 
 
+def get_tablename(obj):
+    if hasattr(obj, '__table__'):
+        " this is a class "
+        table = obj.__table__
+        schema = table.schema or obj.metadata.bind.url.database
+        name = table.name
+    elif hasattr(obj, 'schema'):
+        " this is a table "
+        schema = obj.schema or obj.metadata.bind.url.database
+        name = obj.name
+    else:
+        return None
+    return '%s.%s' % (schema, name)
+
+
 class EngineHandler(ConnectionHandler):
     def __init__(self, databases):
         if not databases:
