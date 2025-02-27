b134b3a26fafc92e33bd3bd881cb3bb43d8ac5a8
David Welte <dwelte@devhub.com>
2025-02-27 14:00:31 -0800

    Fix the version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages, setup
 
 setup(name='baph',
-      version='0.3.4',
+      version='2.0.2',
       install_requires=[
           'Coffin',
           'Django >= 1.5',
