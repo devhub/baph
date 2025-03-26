03b26bec12cbfb62b63a4224b0da233ab2d90f6d
Daniel Rust <dlrust@gmail.com>
2025-03-25 20:51:38 -0700

    Remove Message

--- baph/core/management/commands/runserver.py
+++ baph/core/management/commands/runserver.py
@@ -1,7 +1,6 @@
 """Runserver for Baph.
 
-This is a modified version of the runserver command that allows for the use of the
-Message class.
+This is a modified version of the runserver command.
 """
 
 from django.contrib.staticfiles.management.commands import runserver
@@ -9,21 +8,7 @@ from django.core.servers.basehttp import WSGIRequestHandler
 from django.utils import autoreload
 
 from baph.core.management.new_base import BaseCommand
-from baph.core.management.validation import get_validation_errors
 
-
-class Message(object):
-
-  def __init__(self, *args, **kwargs):
-    self.raw_header_names = set()
-    WSGIRequestHandler.MessageClass.__init__(self, *args, **kwargs)
-
-  def isheader(self, line):
-    i = line.find(':')
-    if i > 0:
-      self.raw_header_names.add(line[:i])
-      return line[:i].lower()
-    return None
 
 def get_environ(self):
   for k, v in self.headers.items():
@@ -32,11 +17,9 @@ def get_environ(self):
 
   environ = super(WSGIRequestHandler, self).get_environ()
   environ['RAW_URI'] = self.path
-  environ['RAW_HEADER_NAMES'] = self.headers.raw_header_names
   return environ
 
 WSGIRequestHandler.get_environ = get_environ
-WSGIRequestHandler.MessageClass = Message
 
 
 class Command(BaseCommand, runserver.Command):
