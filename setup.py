17558235e4c8172fd2a8472a5c33f5c07463795d
Daniel Rust <dlrust@gmail.com>
2025-04-03 12:31:07 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages, setup
 
 setup(name='baph',
-      version='2.0.2',
+      version='2.0.3',
       install_requires=[
           'Coffin',
           'Django >= 1.5',
