72e04a8ad6f3621a599ca60353c0ab25e20bb26e
Daniel Rust <dlrust@gmail.com>
2025-04-03 12:30:14 -0700

    py3 runserver support (#70)
    * py3 Unused import
    
    * Remove Message and raw - unused

--- baph/core/management/commands/runserver.py
+++ baph/core/management/commands/runserver.py
@@ -1,26 +1,14 @@
-from cStringIO import StringIO
-import mimetools
+"""Runserver for Baph.
+
+This is a modified version of the runserver command.
+"""
 
 from django.contrib.staticfiles.management.commands import runserver
 from django.core.servers.basehttp import WSGIRequestHandler
 from django.utils import autoreload
 
 from baph.core.management.new_base import BaseCommand
-from baph.core.management.validation import get_validation_errors
 
-
-class Message(mimetools.Message):
-
-  def __init__(self, *args, **kwargs):
-    self.raw_header_names = set()
-    mimetools.Message.__init__(self, *args, **kwargs)
-
-  def isheader(self, line):
-    i = line.find(':')
-    if i > 0:
-      self.raw_header_names.add(line[:i])
-      return line[:i].lower()
-    return None
 
 def get_environ(self):
   for k, v in self.headers.items():
@@ -29,11 +17,9 @@ def get_environ(self):
 
   environ = super(WSGIRequestHandler, self).get_environ()
   environ['RAW_URI'] = self.path
-  environ['RAW_HEADER_NAMES'] = self.headers.raw_header_names
   return environ
 
 WSGIRequestHandler.get_environ = get_environ
-WSGIRequestHandler.MessageClass = Message
 
 
 class Command(BaseCommand, runserver.Command):
